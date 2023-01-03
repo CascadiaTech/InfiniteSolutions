c73b569770cccdb2b62bf466f15640b640d66291
Your Name <you@example.com>
2023-01-03 09:57:54 -0800

    initial commit

---
+++ components/Header/HeaderComponent.tsx
@@ -0,0 +1,62 @@
+//import 'tw-elements'
+import "tailwindcss-elevation";
+//import { Stickynavbar } from 'flowbite-react'
+
+//const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { ConnectWallet } from '../Web3Modal/WalletConnect'
+export default function HeaderComponent() {
+  //    backgroundImage: `url(${backgroundimage})`,
+
+  return (
+    <div>
+      <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <div className="flex md:order-2">
+            <ConnectWallet></ConnectWallet>
+          </div>
+          <div
+            className="justify-between items-center w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul className="flex flex-row p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link href="/">
+                  <p
+                    className=" cursor-pointer block py-2 pr-4 pl-3 text-black bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                    aria-current="page"
+                  >
+                    Home{" "}
+                  </p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/Dapp/ExchangePage">
+                  <p className=" cursor-pointer block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    Exchange
+                  </p>
+                </Link>
+              </li>
+              <li>
+              <Link href="/AboutUs/AboutusPage">
+                  <p className=" cursor-pointer block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    About Us
+                  </p>
+                </Link>
+              </li>
+              <li>
+              <Link href="/ContactUs/ContactUsForm">
+                  <p className=" cursor-pointer block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    Contact Us
+                  </p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}', 
+    "./public/**/*.html",
+    "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite/**/*.js",
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    screens: {
+      sm: '450px',
+      md: '700px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      height: {
+        '1000': '1000px',
+      },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      }
+    },
+  },
+  variants: {
+    animation: ["motion-safe"]
+},
+  plugins: [require('tailwindcss-elevation')(['responsive']), require('tw-elements/dist/plugin'), require("flowbite/plugin")],
+}
+
